deb92d7ee4acb981d6a8442a6132d89476f36b60
Ray Bell <rayjohnbell0@gmail.com>
2024-05-30 23:14:09 -0400

    Update chat_pandas_df.py

--- streamlit_agent/chat_pandas_df.py
+++ streamlit_agent/chat_pandas_df.py
@@ -1,6 +1,6 @@
 from langchain.agents import AgentType
 from langchain_experimental.agents import create_pandas_dataframe_agent
-from langchain.callbacks import StreamlitCallbackHandler
+from langchain_community.callbacks import StreamlitCallbackHandler
 from langchain.chat_models import ChatOpenAI
 import streamlit as st
 import pandas as pd
